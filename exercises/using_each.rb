078251cc2b1633605d32328e143ec9f890065845
Dani Coleman <danicoleman00@gmail.com>
2021-11-27 15:14:35 -0700

    add warm_up, eexercises and vocabulary folders

---
+++ exercises/using_each.rb
@@ -0,0 +1,7 @@
+# Write code using .each to iterate
+# over the students collection.
+# NOTE: you will use 'puts' within the block to output
+# your return values to the terminal.
+
+students = ["Megan", "Bob", "Mike"]
+# your code below
